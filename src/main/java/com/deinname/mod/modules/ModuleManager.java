fd7432c55553aa93b3fd2f29959aa62d236461b7
heehee682 <j1sjosub25owhc@sql.vezra.store>
2026-07-21 20:17:09 +0200

    Update ModuleManager.java

--- src/main/java/com/deinname/mod/modules/ModuleManager.java
+++ src/main/java/com/deinname/mod/modules/ModuleManager.java
@@ -3,9 +3,6 @@ package com.deinname.mod.modules;
 import com.deinname.mod.modules.impl.AutoClickerModule;
 import com.deinname.mod.modules.impl.FullbrightModule;
 import com.deinname.mod.modules.impl.SprintModule;
-import com.deinname.mod.modules.impl.VelocityModule;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,18 +13,6 @@ public class ModuleManager {
     public static void init() {
         modules.add(new SprintModule());
         modules.add(new FullbrightModule());
-        modules.add(new VelocityModule());
         modules.add(new AutoClickerModule());
-    }
-
-    @SubscribeEvent
-    public static void onClientTick(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            for (Module module : modules) {
-                if (module.isToggled()) {
-                    module.onUpdate();
-                }
-            }
-        }
     }
 }
